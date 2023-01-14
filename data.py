cd551cc87aa6dfa8e8b8d5425e145831fd568ef6
Oscar Pedreño <pedreno.oscar@outlook.com>
2023-01-14 11:58:44 +0100

    add celebrities to data.py

---
+++ data.py
@@ -0,0 +1,62 @@
+data = [
+    {
+    'name':"Cristiano Ronaldo",
+    'follower_count':"533 M" ,
+    'description':"Footbaler",
+    'country':"Portugal",
+    },
+    {
+    'name':"Lionel Messi",
+    'follower_count':"419 M" ,
+    'description':"Footbaler",
+    'country':"Argentina",
+    },
+    {
+    'name':"Kylie Jenner",
+    'follower_count':"376 M" ,
+    'description':"Television Personality",
+    'country':"United States",
+    },
+    {
+    'name':"Selena Gomez",
+    'follower_count':"367 M" ,
+    'description':"Musician",
+    'country':"United States",
+    },
+    {
+    'name':"Dwayne Johnson",
+    'follower_count':"355 M" ,
+    'description':"Actor",
+    'country':"United States",
+    },
+    {
+    'name':"Ariana Grande",
+    'follower_count':"347 M" ,
+    'description':"Musician",
+    'country':"United States",
+    },
+    {
+    'name':"Kim Kardashian",
+    'follower_count':"338 M" ,
+    'description':"Television Personality",
+    'country':"United States",
+    },
+    {
+    'name':"Beyonce",
+    'follower_count':"288 M" ,
+    'description':"Musician",
+    'country':"United States",
+    },
+    {
+    'name':"Khloe Kardashian",
+    'follower_count':"283 M" ,
+    'description':"Television Personality",
+    'country':"United States",
+    },
+    {
+    'name':"Justin Bieber",
+    'follower_count':"271 M" ,
+    'description':"Musician",
+    'country':"Canada",
+    },
+]
